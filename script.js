fdbb6bcc23ea9be14203cb1b5d2790b2f7e77102
Yahyobek1505 <yahyohonorinboyev0105@gmail.com>
2023-12-16 22:32:03 +0500

    Homework is almost done

--- script.js
+++ script.js
@@ -11,14 +11,37 @@
 // }
 // console.log(sum);
 
-// <=============================> 2-masala <=========================>
+// <============================> 2-masala <=========================>
 
-// let num = 123;
+// let son = 12345;
 // let counter = 0;
-//   //  console.log(num % 10);
 
-// while (num >= 1) {
+// while (son !== 0) {
+//     son = Math.trunc(son / 10);
+//     counter++;
+// }
 
+// console.log("Berilgan sonning xonalari: " + counter);
 
-// } 
-// console.log(counter);
+// <============================> 3-masala <=========================>
+
+// let a = 5; //prompt("Istalgan sonni kirting")
+// while ( i <= a ) {
+//   sum *= i;
+//   i++;
+// }
+
+// console.log(sum);
+
+// <============================> 4-masala <=========================>
+
+// let password = "123";
+
+// let enterPassword = prompt("Parolni kiriting:");
+
+// while (enterPassword !== password) {
+//   console.log("Noto'g'ri parol! Qayta urinib ko'ring.");
+//   enterPassword = prompt("Parolni kiriting:");
+// }
+// alert("Parol to'g'ri! Xush kelibsiz!");
+// console.log("Parol to'g'ri! Xush kelibsiz!");
