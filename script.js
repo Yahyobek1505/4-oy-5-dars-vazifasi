b3e6eb5c66f755b54a889f56aba4450e791b67c2
Yahyobek1505 <yahyohonorinboyev0105@gmail.com>
2023-12-16 15:13:55 +0500

    Homework is started.

---
+++ script.js
@@ -0,0 +1,24 @@
+// <=============================> 1-masala <=========================>
+
+// let a = 1;
+// let b = 10;
+// let i = 1;
+// let sum = a;
+
+// while (i <= b) {
+//   sum *= i;
+//   i++;
+// }
+// console.log(sum);
+
+// <=============================> 2-masala <=========================>
+
+// let num = 123;
+// let counter = 0;
+//   //  console.log(num % 10);
+
+// while (num >= 1) {
+
+
+// } 
+// console.log(counter);
